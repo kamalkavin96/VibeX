27c81bd8540bb9ca24e7dace52cb1eb516e16fb4
kamalkavin96 <kamalkavin68@gmail.com>
2025-12-29 21:47:35 +0530

    unused imports removed

--- VibeXBackend/src/main/java/com/kamalkavin96/VibeXBackend/repository/AlbumRepository.java
+++ VibeXBackend/src/main/java/com/kamalkavin96/VibeXBackend/repository/AlbumRepository.java
@@ -4,8 +4,6 @@ import com.kamalkavin96.VibeXBackend.model.Album;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
 public interface AlbumRepository extends JpaRepository<Album, Long> {
 }
