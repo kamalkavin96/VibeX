987c00c4f2477de2d2dabb022f6bd48ac646ca7a
kamalkavin96 <kamalkavin68@gmail.com>
2025-12-29 10:18:39 +0530

    initial commit

---
+++ VibeXBackend/src/main/java/com/kamalkavin96/VibeXBackend/VibeXBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kamalkavin96.VibeXBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeXBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibeXBackendApplication.class, args);
+	}
+
+}
---
+++ VibeXBackend/src/test/java/com/kamalkavin96/VibeXBackend/VibeXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamalkavin96.VibeXBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeXBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
